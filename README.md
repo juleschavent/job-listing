b6fe3911a8fee6e6616d73015709378d6032ac11
Jules Chavent <juleschavent@gmail.com>
2021-06-29 15:44:54 +0200

    final push

--- README.md
+++ README.md
@@ -1,27 +1,178 @@
-# JobListing
+# JobListing with Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.4.
+## Working with json file 
+The objective is to creat a listing preview working with json file.
+Close to nothing should be hardcoded in HTML with the use of NGFOR etc.
+The user must be able to filter by tags and edit dynamicly wich tag to remove from the filter.
+The user must be able to change the display from a list to a grid on medium and larger screen.
+When on mobile, the option to change the display must be hidden for the items to be only shown as a list.
 
-## Development server
+## Improvment
+Logic has been refactored and optimized as best as I could. (old code before refactoring bellow).
+In depth NG logistics could be better used with multiple components and shared services.
+CSS could be more organized with a better use of BEM and comments.
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+## Comments
+There is a lot (too much) comment on TS file, it's here for reference and to explain each step of the logic for people who review the code. It wouldn't be that much detailed in a production environment.
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-## Build
+ ## Old logique that has been updated
+ ### is here for reference and archive
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+//Set a methode that filter by level
+filterLevel(el: any) {
+  let tempList = [];
+  if (this.isFiltered === false) {
+    for (const item of this.listItem) {
+      if (item.level == el) {
+        tempList.push(item);
+      }
+    }
+  } else {
+    tempList = data;
+  }
+  this.listItem = tempList;
+  this.isFiltered = !this.isFiltered;
+}
 
-## Running unit tests
+Set a methode that filter by language
+filterLang(el: any) {
+  let tempList = [];
+  if (this.isFiltered === false) {
+    for (const item of this.listItem) {
+      for (const lang of item.languages) {
+        if (lang == el) {
+          tempList.push(item);
+        }
+      }
+    }
+  } else {
+    tempList = data;
+  }
+  this.listItem = tempList;
+  this.isFiltered = !this.isFiltered;
+}
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+//Set a methode that filter by tool
+filterTool(el: any) {
+  let tempList = [];
+  if (this.isFiltered === false) {
+    for (const item of this.listItem) {
+      for (const tool of item.tools) {
+        if (tool == el) {
+          tempList.push(item);
+        }
+      }
+    }
+  } else {
+    tempList = data;
+  }
+  this.listItem = tempList;
+  this.isFiltered = !this.isFiltered;
+}
 
-## Running end-to-end tests
+____________________PREVIOUS FILTER METHOD____________________________//
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+//Set a methode that filter by role with an argument
+filterRole(el: any) {
+  // create an empty array to store the new list
+  let tempList = [];
+  // for every item of the current list
+  for (const item of this.listItem) {
+    // if this item has the same property/name as the argument
+    if (item.role == el) {
+      // push item to the temporary array
+      tempList.push(item);
+    }
+  }
+  // set the final list to be equal to the temporary list
+  this.listItem = tempList;
+  // push the element that is clicked to filters array
+  this.filters.push(el);
+  // set to true to display in HTML
+  this.isFiltered = true;
+}
 
-## Further help
+//Set a methode that filter by level
+filterLevel(el: any) {
+  let tempList = [];
+  for (const item of this.listItem) {
+    if (item.level == el) {
+      tempList.push(item);
+    }
+  }
+  this.listItem = tempList;
+  this.filters.push(el);
+  this.isFiltered = true;
+}
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+//Set a methode that filter by language
+filterLang(el: any) {
+  let tempList = [];
+  for (const item of this.listItem) {
+    for (const lang of item.languages) {
+      if (lang == el) {
+        tempList.push(item);
+      }
+    }
+  }
+  this.listItem = tempList;
+  this.filters.push(el);
+  this.isFiltered = true;
+}
+
+//Set a methode that filter by tool
+filterTool(el: any) {
+  let tempList = [];
+  for (const item of this.listItem) {
+    for (const tool of item.tools) {
+      if (tool == el) {
+        tempList.push(item);
+      }
+    }
+  }
+  this.listItem = tempList;
+  this.filters.push(el);
+  this.isFiltered = true;
+}
+
+// create a method that display the right array when you remove a tag from filter
+removeRole(tag: any) {
+  // variable is equal to the index of tag
+  let tagIndex = this.filters.indexOf(tag);
+  // indexOf return -1 if it doesn't exist on the array, so if it's not equal to -1 it means it exists
+  if (tagIndex != -1) {
+    // remove the tag at the current index from the filters array
+    this.filters.splice(tagIndex, 1);
+
+    // loop on every object of the main json file
+    for (const obj of data) {
+      // if object.role isn't the same as the removed tag
+      if (obj.role != tag) {
+        // then push to the list the object
+        this.listItem.push(obj);
+        console.log(obj);
+      }
+
+      // if object.level isn't the same as the removed tag
+      if (obj.level != tag) {
+        // then push to the list the object
+        this.listItem.push(obj);
+        console.log(obj);
+      }
+
+      // if object.languages isn't the same as the removed tag
+      if (obj.languages != tag) {
+        // then push to the list the object
+        this.listItem.push(obj);
+        console.log(obj);
+      }
+    }
+  }
+  // if the first item ot the array is undefined meaning it's empty
+  if (this.filters[0] === undefined) {
+    // then run the clearFilters() method
+    this.clearFilters();
+  }
+}
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,54 +7,95 @@ import data from '../../assets/json/data.json';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  //Set a new array that takes all the data from our json file
+  // set a new array that takes all the data from our json file
   listItem = data;
   // create a bool that give the option to modify what the method does
   isFiltered = false;
   // create an array that show tags you've clicked
   filters: string[] = [];
+  // create a bool that give the option to modify the display
+  isListed = false;
 
-  // create a method that display the right array when you remove a tag from filter
-  removeRole(tag: any) {
-    // variable is equal to the index of tag
-    let tagIndex = this.filters.indexOf(tag);
-    // indexOf return -1 if it doesn't exist on the array, so if it's not equal to -1 it means it exists
-    if (tagIndex != -1) {
-      // remove the tag at the current index from the filters array
-      this.filters.splice(tagIndex, 1);
+  // Methode with an argument that will be defined in HTML
+  filter(el: any) {
+    // set a temporary array to store the new list
+    let tempList = [];
+    // push the clicked tag to the filters array. It will be use for display in HTML
+    this.filters.push(el);
+    // set a on/off to tell if there is no filter use
+    this.isFiltered = true;
 
-      // loop on every object of the main json file
-      for (const obj of data) {
-        // if object.role isn't the same as the removed tag
-        if (obj.role != tag) {
-          // then push to the list the object
-          this.listItem.push(obj);
-          console.log(obj);
+    // loop on every element of the current list
+    for (const element of this.listItem) {
+      // if the html argument (el) is equal to the value of the key (role)
+      if (el === element.role) {
+        // then push to the temporary list the element
+        tempList.push(element);
+      }
+      // if the html argument (el) is equal to the value of the key (level)
+      if (el === element.level) {
+        // then push to the temporary list the element
+        tempList.push(element);
+      }
+      // loop on every element of the current key (languages)
+      for (let lang of element.languages) {
+        // if the html argument (el) is equal to the value of element (lang)
+        if (el === lang) {
+          // then push to the temporary list the element
+          tempList.push(element);
         }
-
-        // if object.level isn't the same as the removed tag
-        if (obj.level != tag) {
-          // then push to the list the object
-          this.listItem.push(obj);
-          console.log(obj);
-        }
-
-        // if object.languages isn't the same as the removed tag
-        if (obj.languages != tag) {
-          // then push to the list the object
-          this.listItem.push(obj);
-          console.log(obj);
+      }
+      // loop on every element of the current key (tools)
+      for (let tool of element.tools) {
+        // if the html argument (el) is equal to the value of element (tool)
+        if (el === tool) {
+          // then push to the temporary list the element
+          tempList.push(element);
         }
       }
     }
-    // if the first item ot the array is undefined meaning it's empty
-    if (this.filters[0] === undefined) {
-      // then run the clearFilters() method
-      this.clearFilters();
-    }
+    // finally set the current list equal to the temp list
+    this.listItem = tempList;
+    window.scrollTo(0, 0);
   }
 
-  
+  // methode that remove a tag and update the list accordingly
+  removeTag(tag: any) {
+    // creat a tomporary list to work with
+    let tempList = [];
+    // creat a variable to simplify the use of indexOf()
+    let tagIndex = this.filters.indexOf(tag);
+
+    // remove clicked tag from filter list & if list is empty then run the method clearFilters()
+    this.filters.splice(tagIndex, 1);
+    if (this.filters[0] === undefined) {
+      this.clearFilters();
+    } 
+    // if list is not empty
+    else {
+      // loop on every tags in the filter array
+      for (const tag of this.filters) {
+        // loop on every object of our main json file
+        for (const obj of data) {
+          // if the tag we are removing (meaning we want to show object that didn't have this tag) is equal to any of the key value of our current object
+          if (
+            tag === obj.role ||
+            tag === obj.level ||
+            tag === obj.languages[0] ||
+            tag === obj.languages[1] ||
+            tag === obj.languages[2] ||
+            tag === obj.tools[0] ||
+            tag === obj.tools[1]
+          ) {
+            // then push this object to our temporary list
+            tempList.push(obj);
+          }
+        }
+      }
+      // finally, copy the tomporary list onto the real list
+      this.listItem = tempList;
+    }
+  }
 
   // a method that reset the list
   clearFilters() {
@@ -66,129 +107,14 @@ export class ListComponent implements OnInit {
     this.listItem = data;
   }
 
+  // Methode that switch when the type of display is changed
+  displayType() {
+    this.isListed = !this.isListed;
+    console.log(this.isListed);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
 }
 
-//OLD CODE
-
-// //Set a methode that filter by level
-// filterLevel(el: any) {
-//   let tempList = [];
-//   if (this.isFiltered === false) {
-//     for (const item of this.listItem) {
-//       if (item.level == el) {
-//         tempList.push(item);
-//       }
-//     }
-//   } else {
-//     tempList = data;
-//   }
-//   this.listItem = tempList;
-//   this.isFiltered = !this.isFiltered;
-// }
-
-//Set a methode that filter by language
-// filterLang(el: any) {
-//   let tempList = [];
-//   if (this.isFiltered === false) {
-//     for (const item of this.listItem) {
-//       for (const lang of item.languages) {
-//         if (lang == el) {
-//           tempList.push(item);
-//         }
-//       }
-//     }
-//   } else {
-//     tempList = data;
-//   }
-//   this.listItem = tempList;
-//   this.isFiltered = !this.isFiltered;
-// }
-
-// //Set a methode that filter by tool
-// filterTool(el: any) {
-//   let tempList = [];
-//   if (this.isFiltered === false) {
-//     for (const item of this.listItem) {
-//       for (const tool of item.tools) {
-//         if (tool == el) {
-//           tempList.push(item);
-//         }
-//       }
-//     }
-//   } else {
-//     tempList = data;
-//   }
-//   this.listItem = tempList;
-//   this.isFiltered = !this.isFiltered;
-// }
-
-
-
-
-//____________________PREVIOUS FILTER METHOD____________________________//
-
-// //Set a methode that filter by role with an argument
-// filterRole(el: any) {
-//   // create an empty array to store the new list
-//   let tempList = [];
-//   // for every item of the current list
-//   for (const item of this.listItem) {
-//     // if this item has the same property/name as the argument
-//     if (item.role == el) {
-//       // push item to the temporary array
-//       tempList.push(item);
-//     }
-//   }
-//   // set the final list to be equal to the temporary list
-//   this.listItem = tempList;
-//   // push the element that is clicked to filters array
-//   this.filters.push(el);
-//   // set to true to display in HTML
-//   this.isFiltered = true;
-// }
-
-// //Set a methode that filter by level
-// filterLevel(el: any) {
-//   let tempList = [];
-//   for (const item of this.listItem) {
-//     if (item.level == el) {
-//       tempList.push(item);
-//     }
-//   }
-//   this.listItem = tempList;
-//   this.filters.push(el);
-//   this.isFiltered = true;
-// }
-
-// //Set a methode that filter by language
-// filterLang(el: any) {
-//   let tempList = [];
-//   for (const item of this.listItem) {
-//     for (const lang of item.languages) {
-//       if (lang == el) {
-//         tempList.push(item);
-//       }
-//     }
-//   }
-//   this.listItem = tempList;
-//   this.filters.push(el);
-//   this.isFiltered = true;
-// }
-
-// //Set a methode that filter by tool
-// filterTool(el: any) {
-//   let tempList = [];
-//   for (const item of this.listItem) {
-//     for (const tool of item.tools) {
-//       if (tool == el) {
-//         tempList.push(item);
-//       }
-//     }
-//   }
-//   this.listItem = tempList;
-//   this.filters.push(el);
-//   this.isFiltered = true;
-// }
